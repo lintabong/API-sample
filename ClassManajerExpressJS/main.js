de068dc76c3557396f4011c6d7e85a64a68deb51
Lintang Bongkar Girana <96236126+lintabong@users.noreply.github.com>
2022-09-26 15:50:20 +0700

    Update main.js

--- ClassManajerExpressJS/main.js
+++ ClassManajerExpressJS/main.js
@@ -1,65 +1,19 @@
-const { initializeApp } = require('firebase/app');
-const { getDatabase, ref , get, set, goOffline } = require('firebase/database');
-
 const express = require('express')
-const bodyParser = require('body-parser')
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCAV4IZbWdnBo__3MQFl9nfHY016sqP6hY",
-    authDomain: "classmanagerapps.firebaseapp.com",
-    projectId: "classmanagerapps",
-    storageBucket: "classmanagerapps.appspot.com",
-    messagingSenderId: "1023940208070",
-    appId: ""
-  };
-
-
-initializeApp(firebaseConfig)
-const db = getDatabase();
-
-app.use(bodyParser.json());
-
-app.get('/uname', (req, res) => {
-
-  var username;
-  get(ref(db, 'user/student')).then((snap) => {
-      if (snap.exists()){
-          username = snap.val()
-      }
-      res.send(username)
-  });
-});
-
-app.post('/login', (req, res) => {
-  var body = req.body
-  
-  res.send(body)
-
-});
-
-app.post('/adduser', (req, res) => {
-  var adduser = set(ref(db, 'new/val'), req.body)
-
-  if (adduser){
-    res.send({
-      "status":"success",
-      "content": req.body
-    })
-  } else {
-    res.send("gagal")
-  }
-
-  
+// HOME
+app.get('/', function(req, res){
+  res.send({"message": "index"})
 })
 
+// UNSECURE ROUTE
+app.use('/', require('./controllers/all_user'))
+app.use('/', require('./controllers/login'))
+app.use('/', require('./controllers/register'))
+
+
+// START PROGRAM
 app.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
 });
-
-
-
-
-
- 
