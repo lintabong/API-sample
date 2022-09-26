fd83190cdc72a3d39f66c0314e99093e7a7ff10f
Lintang Bongkar Girana <96236126+lintabong@users.noreply.github.com>
2022-09-26 15:51:17 +0700

    Create config.js

---
+++ ClassManajerExpressJS/firebaseConfig/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyCAV4IZbWdnBo__3MQFl9nfHY016sqP6hY",
+    authDomain: "classmanagerapps.firebaseapp.com",
+    projectId: "classmanagerapps",
+    storageBucket: "classmanagerapps.appspot.com",
+    messagingSenderId: "1023940208070",
+    appId: "1:10239401021732321"
+};
+
+
+module.exports = config
