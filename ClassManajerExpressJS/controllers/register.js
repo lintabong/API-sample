937e1a070ea1ed6b8cffb315df4b553a94c78e64
Lintang Bongkar Girana <96236126+lintabong@users.noreply.github.com>
2022-10-01 00:10:16 +0700

    Update register.js

--- ClassManajerExpressJS/controllers/register.js
+++ ClassManajerExpressJS/controllers/register.js
@@ -3,67 +3,82 @@ const express = require('express')
 
 // init db
 const { initializeApp } = require('firebase/app')
-const { getDatabase, ref , get, set } = require('firebase/database')
+const { getDatabase, ref , set, get } = require('firebase/database')
 
 const firebase_config = require('../firebaseConfig/config')
 
 initializeApp(firebase_config)
 const db = getDatabase();
 
-// main func
+// include functions
+const { randomStr } = require('../functions/random_str')
+
+// main
 var Register = express.Router()
 
-Register.post('/registerstudent', (req, res) => {
-    var i = 0
-    get(ref(db, 'user/student')).then((snap) => {
-      var users = snap.val()
-      
-      for (const user in users){
-        i++
-      }
-      i++
-      
-      var adduser = set(ref(db, 'user/student/' + i.toString()), req.body)
-  
-      if (adduser){
-        res.send({
-          "status":"success",
-          "content" : req.body
-        })
-      } else {
-        res.send({
-          "status":"gagal",
-          "content" : req.body
-        })
-      }
+Register.post('/register', async (req, res) => {
+
+  // get data
+  const username  = req.body.username
+  const email     = req.body.email
+  const password  = req.body.password
+  const type      = req.body.type
+
+  // check payload
+  if (!username || !email || !password || !type){
+
+    res.status(400).send({
+      'message' : 'null payload'
     })
-  })
-  
-  Register.post('/registerteacher', (req, res) => {
-    
-    var i = 0
-    get(ref(db, 'user/teacher')).then((snap) => {
-      var users = snap.val()
+
+    return 
+  } 
+
+  // check duplicate email
+  duplicated_email = false
+  await get(ref(db, 'user/' + type.toString() + '/')).then((snap) => {
+
+    emailList = snap.val()
+
+    for (const address in emailList){
       
-      for (const user in users){
-        i++
+      if (eval('emailList.' + address + '.email').toString() == email.toString()){
+        duplicated_email = true
       }
-      i++
-      
-      var adduser = set(ref(db, 'user/teacher/' + i.toString()), req.body)
-  
-      if (adduser){
-        res.send({
-          "status":"success",
-          "content" : req.body
-        })
-      } else {
-        res.send({
-          "status":"gagal",
-          "content" : req.body
-        })
-      }
-    })
+    }
   })
 
-  module.exports = Register
+  if (duplicated_email == true){
+
+    res.send({
+      "message"  :"duplicate email"
+    })
+
+    return
+  }
+
+  // construct json
+  const value = {
+    "username"  : username,
+    "password"  : password,
+    "email"     : email
+  }
+
+  const register = set(ref(db, 'user/' + type + '/' + randomStr(30)), value)
+  
+  if (register){
+    res.send({
+      "status":"success",
+      "content" : value
+    })
+  } else {
+    res.send({
+      "status":"failed to register",
+      "content" : value
+    })
+
+    return
+  }
+})
+
+module.exports = Register
